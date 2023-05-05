2288e6006534c1071a8d8ca794abed1f56524132
alanantedoro <alanantedoro@gmail.com>
2023-05-05 16:26:30 -0300

    Usando Pokeapi

---
+++ PNT2-Proyecto-final/vue-project/src/components/views/Pokemons.vue
@@ -0,0 +1,35 @@
+<script>
+    
+export default {
+    data () {
+    return {
+        pokemonsParse: [],
+        pokemons: [],
+        }
+    },
+    created: async function() {
+        const pokemons = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0');                 
+        this.pokemons = await pokemons.json();
+        this.pokemonsParse = this.pokemons.results
+        return pokemonsParse;
+        }
+    
+};
+
+</script>
+<template>
+    <h1>Pokemons</h1>
+    <div class="card-group">
+    <div v-for="(item, index) in pokemonsParse">
+        <div class="card" style="width: 8rem;">
+            <div class="card-body">
+            <h5 class="card-title">{{ item.name}}</h5>
+<!-- hacer un *to para el hipervinculo pasandole el id del parametro url del json pokemons -->
+            <a href="#" class="btn btn-primary">Info</a>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+<style scoped>
+</style>
--- PNT2-Proyecto-final/vue-project/src/main.js
+++ PNT2-Proyecto-final/vue-project/src/main.js
@@ -1,14 +1,29 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import {createRouter, createWebHistory} from 'vue-router'
+
+import './assets/main.css'
 
 import App from './App.vue'
-import router from './router'
 
-const app = createApp(App)
+import Index from './components/views/Index.vue'
+import Pokemons from './components/views/Pokemons.vue'
+import Pokemon from './components/views/Pokemon.vue'
 
-app.use(createPinia())
-app.use(router)
 
-app.mount('#app')
+
+const routes = [
+    { path : '/' , component : Index}, 
+    { path : '/Pokemons' , component : Pokemons}, 
+    { path : '/Pokemon/:id' , component : Pokemon},
+]
+
+const router = createRouter(
+    {
+        history : createWebHistory(),
+        routes
+    }
+);
+const pinia = createPinia()
+
+createApp(App).use(pinia).use(router).mount('#app')
